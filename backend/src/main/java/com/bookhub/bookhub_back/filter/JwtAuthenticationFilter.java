0cc7cc377f7ea5f9b9e5013e478040bc8736ae48
Jangsojeong31 <sojung301@naver.com>
2025-07-05 23:40:27 +0900

    [backend]refactor: Config 설정, Security 설정

--- backend/src/main/java/com/bookhub/bookhub_back/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/bookhub/bookhub_back/filter/JwtAuthenticationFilter.java
@@ -1,14 +1,70 @@
 package com.bookhub.bookhub_back.filter;
 
 
+import com.bookhub.bookhub_back.provider.JwtProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
-public class JwtAuthenticationFilter {
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
 
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        try {
+            String authorizationHeader = request.getHeader("Authorization");
+
+            String token = (authorizationHeader != null &&  authorizationHeader.startsWith("Bearer "))
+                    ? jwtProvider.removeBearer(authorizationHeader)
+                    : null;
+
+            if (token == null || !jwtProvider.isValidToken(token)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String loginId = jwtProvider.getLoginIdFromJwt(token);
+            String roles = jwtProvider.getRolesFromJwt(token);
+
+            setAuthenticationContext(request, loginId, roles);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void setAuthenticationContext(HttpServletRequest request, String loginId, String role) {
+        String normalizedRole = role.startsWith("ROLE_") ? role : "ROLE_" + role;
+        GrantedAuthority authority = new SimpleGrantedAuthority(normalizedRole);
+
+        AbstractAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginId, null, List.of(authority));
+
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authenticationToken);
+
+        SecurityContextHolder.setContext(securityContext);
+    }
 }
--- backend/src/main/java/com/bookhub/bookhub_back/provider/JwtProvider.java
+++ backend/src/main/java/com/bookhub/bookhub_back/provider/JwtProvider.java
@@ -1,5 +1,102 @@
 package com.bookhub.bookhub_back.provider;
 
-public class JwtProvider {
+import com.bookhub.bookhub_back.entity.Authority;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    private final Key key;
+    private final int jwtExpirationMs;
+    private final long jwtEmailExpirationMs;
+
+    public int getExpiration() { return jwtExpirationMs; }
+
+    public JwtProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") int jwtExpirationMs,
+            @Value("${jwt.email-expiration-ms}") long jwtEmailExpirationMs
+    ) {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        this.jwtExpirationMs = jwtExpirationMs;
+        this.jwtEmailExpirationMs = jwtEmailExpirationMs;
+    }
+
+    public String generateJwtToken(String loginId, Authority roles) {
+        return Jwts.builder()
+                .claim("loginId", loginId)
+                .claim("role", roles.getAuthorityName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateEmailValidToken(String email, String phoneNumber, String loginId) {
+        return Jwts.builder()
+                .claim("email", email)
+                .claim("phoneNumber", phoneNumber)
+                .claim("loginId", loginId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtEmailExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateEmailValidToken(String email, String loginId) {
+        return Jwts.builder()
+                .claim("email", email)
+                .claim("loginId", loginId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtEmailExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String removeBearer(String bearerToken) {
+        if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
+            throw new RuntimeException("Invalid JWT token format");
+        }
+        return bearerToken.substring("Bearer ".length());
+    }
+
+    public boolean isValidToken(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        JwtParser jwtParser = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build();
+        return jwtParser.parseClaimsJws(token).getBody();
+    }
+
+//    public String getUsernameFromJwt(String token) {
+//        Claims claims = getClaims(token);
+//        return claims.get("username", String.class);
+//    }
+
+    public String getLoginIdFromJwt(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("loginId", String.class);
+    }
+
+    public String getRolesFromJwt(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("role", String.class);
+    }
 }
