e779799982e02f38aaab27045055838fcb4217c2
Jangsojeong31 <sojung301@naver.com>
2025-08-03 00:06:26 +0900

    Backend github action test

--- backend/src/main/java/com/bookhub/bookhub_back/controller/TestController.java
+++ backend/src/main/java/com/bookhub/bookhub_back/controller/TestController.java
@@ -1,16 +1,29 @@
 package com.bookhub.bookhub_back.controller;
 
 import com.bookhub.bookhub_back.common.constants.ApiMappingPattern;
+import com.bookhub.bookhub_back.common.constants.ResponseCode;
+import com.bookhub.bookhub_back.common.constants.ResponseMessage;
+import com.bookhub.bookhub_back.dto.ResponseDto;
+import com.bookhub.bookhub_back.dto.alert.response.AlertResponseDto;
+import com.bookhub.bookhub_back.security.UserPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(ApiMappingPattern.V1 + ApiMappingPattern.AUTH)
 public class TestController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "GitHub Action Test Success!";
+    public ResponseEntity<ResponseDto<String>> hello(
+    ) {
+        ResponseDto<String> responseDto = ResponseDto.success(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, "GitHub Action Test Success!");
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 }
